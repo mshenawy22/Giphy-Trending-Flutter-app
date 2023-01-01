2d07bb75786b52c55831637b3f5574b58d499cda
Jay Shenawy <Jay.shenawy@netxtra.net>
2023-01-01 20:31:28 +0000

    Fetching Trending Images api - Searching Images api

---
+++ lib/Models/GiphsModel.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import '../Entities/giphy_trending.dart';
+import 'base_model.dart';
+import 'package:http/http.dart' as http;
+
+const baseUrl = 'http://api.giphy.com/v1';
+const apiKey = '6AAiytlk5hNB6BixO6EewODsia5MeRxv';
+const integer = '5';
+const offset = '2';
+
+class  GiphsModel extends BaseModel {
+
+  Future<GiphyTrending> fetchImages()  async {
+    final response = await http.get(Uri.parse('$baseUrl/gifs/trending?api_key=${apiKey}&limit=${integer}&offset=${offset}/'));
+    print ('response code ${response.statusCode}');
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return GiphyTrending.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+
+  }
+
+  Future<GiphyTrending> searchImages(String searchString)  async {
+    final response = await http.get(Uri.parse('$baseUrl/gifs/search?api_key=${apiKey}&q=${searchString}&limit=${integer}&offset=${offset}/'));
+    print ('response code ${response.statusCode}');
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return GiphyTrending.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+
+  }
+
+  GiphsModel();
+}
