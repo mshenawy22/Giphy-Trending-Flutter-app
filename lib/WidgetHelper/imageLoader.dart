7398673734db635a63c7e9cff2153a1dcd03e356
Jay Shenawy <Jay.shenawy@netxtra.net>
2023-01-02 18:55:33 +0000

    image loader - shows progress loader while the gif image fully loads

---
+++ lib/WidgetHelper/imageLoader.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class imageLoader extends StatelessWidget {
+  const imageLoader({Key? key ,  required this.imageUrl}) : super(key: key);
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Image.network(
+        imageUrl,
+      loadingBuilder:   (BuildContext context, Widget child,
+          ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) {
+          return child;
+        }
+        return Center(
+          child: CircularProgressIndicator(
+            value: loadingProgress.expectedTotalBytes != null
+                ? loadingProgress.cumulativeBytesLoaded /
+                loadingProgress.expectedTotalBytes!
+                : null,
+          ),
+        );
+      },
+         );
+
+  }
+}
