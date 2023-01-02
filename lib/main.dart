3178dd716d0415e55732e0575dbda1a68badb22d
Jay Shenawy <Jay.shenawy@netxtra.net>
2023-01-02 16:49:49 +0000

    Adding Providers - Adding ListView to display searched images

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Entities/giphy_trending.dart';
+import 'Models/GiphsModel.dart';
+import 'package:easy_search_bar/easy_search_bar.dart';
+
+import 'Models/base_model.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+  MultiProvider(
+      providers :[
+        ChangeNotifierProvider<BaseModel>(
+          create: (_) =>  BaseModel() ,
+          // child: MyApp(),
+        ),
+        ChangeNotifierProvider<GiphsModel>(
+          create: (_) =>  GiphsModel() ,
+          // child: MyApp(),
+        ),
+      ],
+      child : const MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -48,17 +69,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // late Future<GiphyTrending> giphyTrendingAlbum;
+  // late Future<GiphyTrending> giphySearchAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    GiphsModel giphsModel =  GiphsModel();
+    // giphyTrendingAlbum = giphsModel.fetchImages();
+    // giphySearchAlbum = giphsModel.searchImages('girl');
+
   }
 
   @override
@@ -69,47 +90,58 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return
+
+      Consumer<GiphsModel>(builder: (context, model, child)
+        {
+          var children  = <Widget>[] ;
+          int index = 0;
+
+          // print (snapshot.data!.data![0].images!.original!.webp!);
+          if (model.giphySearchAlbum != null ) {
+
+        for (var element in model.giphySearchAlbum!.data!) {
+          index += 1;
+          children.add(Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Text('$index .',
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold))));
+          children.add(
+            Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Image.network(element.images!.original!.webp!)),
+          );
+        }
+      }
+          else {
+            children.add(const CircularProgressIndicator());
+          }
+
+      return  Scaffold(
+              appBar:  EasySearchBar(
+
+                title: const Text('Giphy Sharing app'),
+                
+                onSearch: (value) => model.searchImages(value),
+                 )
+          ,
+          body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child:
+            model.loadingStatus == LoadingStatusE.idle?
+              ListView(
+              children: children ,
+              ):                  // By default, show a loading spinner.
+             const CircularProgressIndicator()
+
+          )
+
+         // This trailing comma makes auto-formatting nicer for build methods.
+          );
+        });
+
   }
 }
