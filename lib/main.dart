83a9110af52f64789799c37be4ea357d77127354
Jay Shenawy <Jay.shenawy@netxtra.net>
2023-01-02 18:55:44 +0000

    remove of redundant code

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'Models/GiphsModel.dart';
 import 'package:easy_search_bar/easy_search_bar.dart';
 
 import 'Models/base_model.dart';
+import 'Screens/home_screen.dart';
+import 'Constants/constants.dart';
 
 
 void main() {
@@ -45,103 +47,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  // late Future<GiphyTrending> giphyTrendingAlbum;
-  // late Future<GiphyTrending> giphySearchAlbum;
-
-  @override
-  void initState() {
-    super.initState();
-    GiphsModel giphsModel =  GiphsModel();
-    // giphyTrendingAlbum = giphsModel.fetchImages();
-    // giphySearchAlbum = giphsModel.searchImages('girl');
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return
-
-      Consumer<GiphsModel>(builder: (context, model, child)
-        {
-          var children  = <Widget>[] ;
-          int index = 0;
-
-          // print (snapshot.data!.data![0].images!.original!.webp!);
-          if (model.giphySearchAlbum != null ) {
-
-        for (var element in model.giphySearchAlbum!.data!) {
-          index += 1;
-          children.add(Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              child: Text('$index .',
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold))));
-          children.add(
-            Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                child: Image.network(element.images!.original!.webp!)),
-          );
-        }
-      }
-          else {
-            children.add(const CircularProgressIndicator());
-          }
-
-      return  Scaffold(
-              appBar:  EasySearchBar(
-
-                title: const Text('Giphy Sharing app'),
-                
-                onSearch: (value) => model.searchImages(value),
-                 )
-          ,
-          body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child:
-            model.loadingStatus == LoadingStatusE.idle?
-              ListView(
-              children: children ,
-              ):                  // By default, show a loading spinner.
-             const CircularProgressIndicator()
-
-          )
-
-         // This trailing comma makes auto-formatting nicer for build methods.
-          );
-        });
-
-  }
-}
